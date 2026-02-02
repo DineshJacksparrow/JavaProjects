2672e3a18a5bef11998d57d86e58e4318d6b8f82
RedwoodTechno <consultantdineshkumar@gmail.com>
2026-02-02 22:55:50 +0530

    Initial commit by Mouli

---
+++ src/main/java/com/crypto/democrypto/DemocryptoApplication.java
@@ -0,0 +1,15 @@
+package com.crypto.democrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocryptoApplication.class, args);
+		//created by Mouli
+		DemocryptoApplication d = new DemocryptoApplication();
+	}
+
+}
